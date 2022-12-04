9a9b6d2a1c92e7e7d9ab0774581e755ba6c39ce0
jakubzemla <jakubzemla@gmail.com>
2022-12-04 10:44:52 +0100

    Created RoomForm and CreateView

--- base/admin.py
+++ base/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from base.models import Room
+from base.models import Message
 
 admin.site.register(Room)
+admin.site.register(Message)
