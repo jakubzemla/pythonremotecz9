9c1e644677d4ea6ce6325d6cf2d5003098d63ed5
jakubzemla <jakubzemla@gmail.com>
2023-01-03 11:44:54 +0100

    Create my own form for room creating and room updating

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, FormView, CreateView
+from django.views.generic import TemplateView, ListView, FormView, CreateView, UpdateView, DeleteView
 
 from base.forms import RoomForm
 from base.models import Room
@@ -25,8 +25,21 @@ class RoomsView(ListView):
     model = Room
 
 
-def room(request, id):
-    room = Room.objects.get(id=id)
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    #POST
+    if request.method == "POST":
+        Message.objects.create(
+            body=request.POST.get('body'),
+            room=room,
+            user=request.user
+        )
+        room.participants.add(request.user)
+        room.save()
+        return redirect('room', pk=pk)
+
+    #GET
     messages = room.message_set.all()
     context = {'messages': messages, 'room': room}
     return render(request, template_name='base/room.html', context=context)
@@ -36,6 +49,7 @@ class RoomCreateView(CreateView):
     template_name = 'base/room_form.html'
     form_class = RoomForm
     success_url = reverse_lazy('rooms')
+    extra_context = {'title': 'CREATE ROOM'}
 
     # def form_valid(self, form):
     #     cleaned_data = form.cleaned_data
@@ -44,3 +58,17 @@ class RoomCreateView(CreateView):
     #         description=cleaned_data['description']
     #     )
     #     return super().form_valid(form)
+
+
+class RoomUpdateView(UpdateView):
+    template_name = 'base/room_form.html'
+    form_class = RoomForm
+    success_url = reverse_lazy('rooms')
+    extra_context = {'title': 'UPDATE ROOM'}
+    model = Room
+
+
+class RoomDeleteView(DeleteView):
+    template_name = 'base/room_confirm_delete.html'
+    model = Room
+    success_url = reverse_lazy('rooms')
